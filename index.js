69ef48a04ba0de00d616e9e55ea7361ce3c5e161
ClaySamuelC <claysamuelc@gmail.com>
2021-03-29 17:20:04 -0500

    Add materials to give color and texture to the world

--- index.js
+++ index.js
@@ -6,14 +6,31 @@ const createScene =  () => {
 
   const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
 
+  // world objects
   const ground = BABYLON.MeshBuilder.CreateGround("ground", {width:10, height:10});
   const box = BABYLON.MeshBuilder.CreateBox("box", {});
   const roof = BABYLON.MeshBuilder.CreateCylinder("roof", {diameter: 1.3, height: 1.2, tessellation: 3});
 
+  // world object transformations
   box.position.y = 0.5;
   roof.scaling.x = 0.75;
   roof.rotation.z = Math.PI / 2;
   roof.position.y = 1.22;
+
+  // materials
+  const groundMat = new BABYLON.StandardMaterial("groundMat");
+  const roofMat = new BABYLON.StandardMaterial("roofMat");
+  const boxMat = new BABYLON.StandardMaterial("boxMat");
+
+  // material transformations
+  groundMat.diffuseColor = new BABYLON.Color3.Green();
+  roofMat.diffuseTexture = new BABYLON.Texture("https://assets.babylonjs.com/environments/roof.jpg");
+  boxMat.diffuseTexture = new BABYLON.Texture("https://www.babylonjs-playground.com/textures/floor.png");
+
+  // attach materials to world objects
+  ground.material = groundMat;
+  roof.material = roofMat;
+  box.material = boxMat;
 
   return scene;
 };
