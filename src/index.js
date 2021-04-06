9e2f53524f710915112985d5fe49c0c0a983da98
ClaySamuelC <claysamuelc@gmail.com>
2021-04-06 14:39:54 -0500

    Add car, wheels, and wheel textures

--- src/index.js
+++ src/index.js
@@ -1,119 +1,8 @@
-const createScene = () => {
-  const scene = new BABYLON.Scene(engine);
-
-  const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 10, new BABYLON.Vector3(0, 0, 0));
-  camera.attachControl(canvas, true);
-
-  const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
-
-  // world objects
-  const ground = buildGround();
-  const village = buildVillage();
-
-  return scene;
-};
-
-// Build Functions
-const buildVillage = () => {
-  const smallHouse = buildHouse();
-  const wideHouse = buildHouse(true);
-
-  const houses = [];
-  for (let i = 0; i < 8; i++) {
-    if (i < 5) {
-      houses[i] = smallHouse.createInstance("house" + i);
-      houses[i].position.x = (i - 2.5 + 0.5) * 1.3;
-      houses[i].position.z = -2;
-    } else {
-      houses[i] = wideHouse.createInstance("house" + i);
-      houses[i].position.x = ((i - 5) - 1.5 + 0.5) * 2.5
-      houses[i].position.z = 2;
-    }
-  }
-
-  smallHouse.rotation.y = Math.PI / 2;
-  smallHouse.position.x = -4;
-
-  wideHouse.rotation.y = Math.PI / 2;
-  wideHouse.position.x = 4;
-
-  return houses;
-}
-
-const buildHouse = (isWide = false) => {
-  const box = isWide ? buildBoxWide() : buildBoxRegular();
-  const roof = buildRoof(isWide);
-
-  return BABYLON.Mesh.MergeMeshes([box, roof], true, false, null, false, true);
-};
-
-const buildGround = () => {
-  const groundMat = new BABYLON.StandardMaterial("groundMat");
-  groundMat.diffuseColor = new BABYLON.Color3.Green();
-
-  const ground = BABYLON.MeshBuilder.CreateGround("ground", {width:10, height:10});
-  ground.material = groundMat;
-
-  return ground;
-};
-
-const buildBoxRegular = () => {
-  const boxMat = new BABYLON.StandardMaterial("boxmat");
-  boxMat.diffuseTexture = new BABYLON.Texture("https://assets.babylonjs.com/environments/cubehouse.png");
-
-  faceUV = [
-    new BABYLON.Vector4(0.5, 0.0, 0.75, 1.0), // back
-    new BABYLON.Vector4(0.0, 0.0, 0.25, 1.0), // front
-    new BABYLON.Vector4(0.25, 0.0, 0.5, 1.0), // right
-    new BABYLON.Vector4(0.75, 0.0, 1.0, 1.0) // left
-  ];
-
-  const box = BABYLON.MeshBuilder.CreateBox("box", {faceUV: faceUV, wrap: true});
-  box.material = boxMat;
-
-  box.position.y = 0.5;
-
-  return box;
-};
-
-const buildBoxWide = () => {
-  const boxMat = new BABYLON.StandardMaterial("boxmat");
-  boxMat.diffuseTexture = new BABYLON.Texture("https://assets.babylonjs.com/environments/semihouse.png");
-
-  faceUV = [
-    new BABYLON.Vector4(0.4, 0.0, 0.8, 1.0), // back
-    new BABYLON.Vector4(0.6, 0.0, 1.0, 1.0), // front
-    new BABYLON.Vector4(0.4, 0.0, 0.6, 1.0), // right
-    new BABYLON.Vector4(0.4, 0.0, 0.6, 1.0) // left
-  ];
-
-  const box = BABYLON.MeshBuilder.CreateBox("box", {width: 2, faceUV: faceUV, wrap: true});
-  box.material = boxMat;
-
-  box.position.y = 0.5;
-
-  return box;
-};
-
-const buildRoof = (isWide) => {
-  const roofMat = new BABYLON.StandardMaterial("roofMat");
-  roofMat.diffuseTexture = new BABYLON.Texture("https://assets.babylonjs.com/environments/roof.jpg");
-
-  const roof = BABYLON.MeshBuilder.CreateCylinder("roof", {diameter: 1.3, height: 1.2, tessellation: 3});
-  roof.material = roofMat;
-
-  roof.scaling.x = 0.75;
-  if (isWide) roof.scaling.y = 2;
-  roof.rotation.z = Math.PI / 2;
-  roof.position.y = 1.22;
-
-  return roof;
-};
+import createScene from './scene.js';
 
 const canvas = document.getElementById("renderCanvas"); // Get the canvas element
 const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
-// Add your code here matching the playground format
 const scene = createScene(); // Call the createScene function
 
 // Register a render loop to repeatedly render the scene
@@ -125,3 +14,5 @@ engine.runRenderLoop(function () {
 window.addEventListener("resize", function () {
   engine.resize();
 });
+
+export { canvas, engine };
--- src/scene.js
+++ src/scene.js
@@ -1,4 +1,4 @@
-import {canvas, engine} from './index.js';
+import { canvas, engine } from './index.js';
 
 const createScene = () => {
   const scene = new BABYLON.Scene(engine);
@@ -8,34 +8,70 @@ const createScene = () => {
 
   const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
 
-  const village = createVillage();
+  // const village = createVillage();
   const car = createCar();
+
+  car.position.y = 0.125
+  car.position.z = 2;
+  car.rotation.y = Math.PI / 2;
 
   return scene;
 };
 
+const createVillage = () => {
+  const village = BABYLON.SceneLoader.ImportMeshAsync("", "https://assets.babylonjs.com/meshes/", "village.glb");
+
+  return village;
+};
+
 const createCar = () => {
-  const outline = [
-    new BABYLON.Vector3(-0.3, 0, -0.1),
-    new BABYLON.Vector3(0.2, 0, -0.1),
-  ];
+  const car = BABYLON.MeshBuilder.CreateBox("car", {depth: 2});
 
-  for (let i = 0; i < 20; i++) {
-    outline.push(new BABYLON.Vector3(0.2 * Math.cos(i * Math.PI / 40), 0, 0.2 * Math.sin(i * Math.PI / 40) - 0.1));
-  }
+  const wheelRB = createWheel();
+  wheelRB.parent = car;
+  wheelRB.position.y = -0.5;
 
-  outline.push(new BABYLON.Vector3(0, 0, 0.1));
-  outline.push(new BABYLON.Vector3(-0.3, 0, 0.1));
+  const wheelRF = wheelRB.clone("wheelRF");
+  const wheelLB = wheelRB.clone("wheelLB");
+  const wheelLF = wheelRB.clone("wheelLF");
 
-  const car = BABYLON.MeshBuilder.ExtrudePolygon("car", {shape: outline, depth: 0.2});
+  wheelRB.position.x = 0.65;
+  wheelRB.position.z = -0.7;
 
+  wheelRF.position.x = 0.65;
+  wheelRF.position.z = 0.7;
+
+  wheelLB.position.x = -0.65;
+  wheelLB.position.z = -0.7;
+
+  wheelLF.position.x = -0.65;
+  wheelLF.position.z = 0.7;
+
+  // car.scaling = new BABYLON.Vector3(0.2, 0.2, 0.2);
+  /* height of car: (1 + 0.25) * 0.2 == 0.25
+   to have the car rest on the ground, it should be raised by 0.125 (half the height of the main part of the car minus the height of the ovverreach of the wheels) */
   return car;
 };
 
-const createVillage = () => {
-  const village = BABYLON.SceneLoader.ImportMeshAsync("village", "https://assets.babylonjs.com/meshes/", "village.glb");
+const createWheel = () => {
+  const wheelUV = [];
+  wheelUV[0] = new BABYLON.Vector4(0, 0, 1, 1); // front
+  wheelUV[1] = new BABYLON.Vector4(0, 0.5, 0, 0.5); // side
+  wheelUV[2] = new BABYLON.Vector4(0, 0, 1, 1); // back
 
-  return village;
-}
+  const wheelMat = new BABYLON.StandardMaterial("wheelMat");
+  wheelMat.diffuseTexture = new BABYLON.Texture("https://assets.babylonjs.com/environments/wheel.png");
+  
+  const wheelRB = BABYLON.MeshBuilder.CreateCylinder("wheelRB", {diameter: 0.5, height: 0.25, faceUV: wheelUV});
+  wheelRB.material = wheelMat;
+  
+  wheelRB.rotation.z = Math.PI / 2;
+
+  return wheelRB;
+};
+
+const createWheelAnim = () => {
+  const animWheel = new BABYLON.Animation("wheelAnimation", "rotation.y", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT, BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+};
 
 export default createScene;
